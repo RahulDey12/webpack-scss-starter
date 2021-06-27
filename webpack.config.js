79de79286f24285b5b5167dc6585af3583bb6801
Rahul Dey <rahul900day@gmail.com>
2021-06-28 02:59:16 +0530

    added webpack@5 Support

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,74 @@
-const path = require('path');
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require("terser-webpack-plugin");
+const WebpackBar = require('webpackbar');
 
-module.exports = {
-    entry: './src/js/app.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/app.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
+module.exports = (_env, options) => {
+    const isProduction = options.mode !== 'development';
 
-            {
-                test: /\.(sc|sa|c)ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader',
-                ]
-            },
-
-            {
-                test: /\.(jpe?g|png|webp|ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-                use: [
-                    {
-                        loader: 'file-loader',
+    return {
+        entry: './src/js/app.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'js/app.js'
+        }, 
+    
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
                         options: {
-                            context: path.resolve(__dirname, 'src'),
-                            name: '[path][name]-[hash].[ext]',
-                            publicPath: '../public/',
-                            outputPath: 'public'
+                            presets: ['@babel/preset-env']
                         }
                     }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/app.css',
-        })
-    ],
-    optimization: {
-        minimizer: [
-            new TerserPlugin(),
-            new CssMinimizerPlugin(),
+                },
+    
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        isProduction ? 
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                            options: {
+                                publicPath: '../',
+                            }
+                        } : 
+                        'style-loader',
+                        'css-loader',
+                        'postcss-loader',
+                        'sass-loader'
+                    ]
+                },
+
+                {
+                    test: /\.(jpe?g|png|webp|ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: 'public/[name]-[hash][ext]'
+                    }
+                }
+            ]
+        },
+
+        plugins: [
+            new WebpackBar(),
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: 'css/app.css'
+            })
         ],
-    },
-    devtool: 'source-map'
+
+        optimization: {
+            minimizer: [
+              `...`,
+              new CssMinimizerPlugin(),
+            ],
+          },
+
+        devtool: 'source-map'
+    }
 }
